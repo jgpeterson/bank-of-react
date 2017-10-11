47899bca80b0e987979fe12b21433cac82992bcf
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-11 18:25:27 -0400

    i think i got it working and osmewhat understand

--- src/App.js
+++ src/App.js
@@ -1,63 +1,105 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
-//this is the key for everything we're building 
-//says import this component, but import it as "Router" aka call it "Router" not "Browser Router"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Home from "./components/Home"
-import UserProfile from "./components/UserProfile"
-import AccountBalance from "./components/AccountBalance"
-import Debits from "./components/Debits"
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import DebitsPage from './components/DebitsPage';
+import CreditsPage from './components/CreditsPage';
+
+import axios from 'axios';
+
 class App extends Component {
-  state = {
-    accountBalance: 12345.34,
-    user: {
-      userName: "Bob",
-      memberSince: 1950
-    },
-    debits: []
-  }
-  displayAllDebits = () => {
-    console.log("debits")
-    axios.get(`http://localhost:4000/debits`)
-      .then((response) => {
-        console.log(response)
-        this.setState({
-          debits: response.data
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-  componentWillMount() {
-    this.displayAllDebits()
-  }
-  //have to use below to pass in props
-  //then use AccountBalanceWrapper in the <Switch> below
-  //using this syntax takes state to highestmost level 
-  render() {
-    const AccountBalanceWrapper = () => {
-      return (<AccountBalance accountBalance={this.state.accountBalance} />)
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: {
+        userName: 'jasonvoorhees',
+        memberSince: 1980,
+      },
+      debits: [],
+      credits: [],
     }
-    const UserProfileWrapper = () => {
-      return (<UserProfile userName={this.state.user.userName} memberSince={this.state.user.memberSince} />)
-    }
-    const DebitWrapper = () => {
-      return (<Debits id={this.state.debits.id} description={this.state.debits.description} amount={this.state.debits.amount} date={this.state.debits.date} />)
-    }
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/account" render={AccountBalanceWrapper} />
-          <Route exact path="/user" render={UserProfileWrapper} />
-          <Route exact path="/debits" displayAllDebits={this.displayAllDebits} render={DebitWrapper} />
-        </Switch>
-      </Router>
-    );
   }
+
+  _getDebits = () => {
+    axios.get('http://localhost:4000/debits').then((response) => {
+      const debits = response.data;
+      this.setState({debits});
+    })
+  }
+
+  _getCredits = () => {
+    axios.get('http://localhost:4000/credits').then((response) => {
+      const credits = response.data;
+      this.setState({credits});
+  })
 }
+
+_calculateAccountBalance = () => {
+  const totalCredits = this.state.credits.reduce((totalCredits, credit) => {
+    return totalCredits + credit.amount;
+  }, 0);
+
+  const totalDebits = this.state.debits.reduce((totalDebits, debit) => {
+    return totalDebits + debit.amount;
+  }, 0);
+
+  return totalCredits - totalDebits;
+};
+
+_addNewDebit = (newDebit) => {
+  const debits = [...this.state.debits];
+  debits.push(newDebit);
+  this.setState({debits});
+};
+
+_addNewCredit = (newCredit) => {
+  const credits = [...this.state.credits];
+  credits.push(newCredit);
+  this.setState({credits});
+};
+
+componentWillMount() {
+  this._getDebits();
+  this._getCredits();
+}
+
+render() {
+
+  const accountBalance = this._calculateAccountBalance();
+
+  const HomeComponent = () => (<Home accountBalance={accountBalance}/>);
+  const UserProfileComponent = () => (
+      <UserProfile
+          userName={this.state.currentUser.userName}
+          memberSince={this.state.currentUser.memberSince}/>
+  );
+  const DebitsPageComponent = () => (
+      <DebitsPage
+          debits={this.state.debits}
+          addNewDebit={this._addNewDebit}
+          accountBalance={accountBalance}/>
+  );
+  const CreditsPageComponent = () => (
+      <CreditsPage
+          credits={this.state.credits}
+          addNewCredit={this._addNewCredit}
+          accountBalance={accountBalance}/>
+  );
+
+  return (
+      <Router>
+        <div>
+          <Route exact path="/" render={HomeComponent}/>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+          <Route exact path="/debits" render={DebitsPageComponent}/>
+          <Route exact path="/credits" render={CreditsPageComponent}/>
+        </div>
+      </Router>
+  );
+}
+}
+
 export default App;
